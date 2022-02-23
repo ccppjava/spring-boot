5523b6421ca142b3552d7bbf2f69562ffc22083e
zdeng <zdeng@zdengs-mbp.home>
2022-02-23 22:07:49 +0000

    add simple spring controller

--- src/main/java/herts/demo/DemoApplication.java
+++ src/main/java/herts/demo/DemoApplication.java
@@ -2,12 +2,21 @@ package herts.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class DemoApplication {
+
+	@GetMapping("/")
+	@ResponseBody
+	String home() {
+		return "Demo App 01";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
